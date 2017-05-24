333fd0ae3cdcdbdd8081307efd8ac21b7f2d1eb4
alex lehar <picturesolve@gmail.com>
2017-05-24 10:01:20 -0400

    BlobTest cleanup

--- blobtest.py
+++ blobtest.py
@@ -24,26 +24,7 @@ plt.close('all')
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_axes([.1, .1, .8, .8])
 
-#Path = mpath.Path
-#path_data = [
-#    (Path.MOVETO, (1.58, -2.57)),
-#    (Path.CURVE4, (0.35, -1.1)),
-#    (Path.CURVE4, (-1.75, 2.0)),
-#    (Path.CURVE4, (0.375, 2.0)),
-#    (Path.LINETO, (0.85, 1.15)),
-#    (Path.CURVE4, (2.2, 3.2)),
-#    (Path.CURVE4, (3, 0.05)),
-#    (Path.CURVE4, (2.0, -0.5)),
-#    (Path.CLOSEPOLY, (1.58, -2.57)),
-#    ]
-#codes, verts = zip(*path_data)
-#path = mpath.Path(verts, codes)
-#patch = mpatches.PathPatch(path, facecolor='r', alpha=0.5)
-#ax.add_patch(patch)
 
-# plot control points and connecting lines
-#x, y = zip(*path.vertices)
-#line, = ax.plot(x, y, 'go-')
 
 ax.grid()
 ax.axis('equal')
@@ -99,18 +80,8 @@ def on_press(event):
                        'transPos':transPos,
                        'selected':True,
                        'circle':circ})
-#        path_data = [
-#            (Path.MOVETO, (1.58, -2.57)),
-#            (Path.CURVE4, (0.35, -1.1)),
-#            (Path.CURVE4, (-1.75, 2.0)),
-#            (Path.CURVE4, (0.375, 2.0)),
-#            (Path.LINETO, (0.85, 1.15)),
-#            (Path.CURVE4, (2.2, 3.2)),
-#            (Path.CURVE4, (3, 0.05)),
-#            (Path.CURVE4, (2.0, -0.5)),
-#            (Path.CLOSEPOLY, (1.58, -2.57)),
-#            ]
 
+    
         if len(path_data) == 0:             # If first point just MOVETO
             path_data.append((Path.MOVETO, (xdata, ydata)))
         elif len(path_data) == 1:           # If second point draw LINETO 
