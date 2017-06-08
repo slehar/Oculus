829ea6d575af79e1d9ffc7ab1abacd8c469b8879
Steven Lehar <slehar@gmail.com>
2017-06-08 09:07:46 -0400

    blobtest: fix immutable (tuple) to mutable [list] in path_data

--- blobtest.py
+++ blobtest.py
@@ -14,7 +14,7 @@ ptList = []
 inAPoint = False
 selectedPt = None
 scale = 1.
-ptRad = 0.01
+ptRad = 0.02
 buttonState = False
 path_data = []
 patchList = []
@@ -51,25 +51,25 @@ def extendPolyPath(xdata, ydata):
 
     nPts = len(ptList)
     if nPts == 1:             # If first point just MOVETO
-        path_data.append((Path.MOVETO, (xdata, ydata)))
+        path_data.append([Path.MOVETO, [xdata, ydata]])
         print '  Path.MOVETO (%5.2f, %5.2f)'%(xdata, ydata)
            
     elif nPts == 2:           # If second point draw LINETO
-        path_data.append((Path.LINETO, (xdata, ydata)))
+        path_data.append([Path.LINETO, [xdata, ydata]])
         print '  Path.LINETO (%5.2f, %5.2f)'%(xdata, ydata)
             
     elif nPts == 3:           # If third point draw LINETO and CLOSEPOLY
-        path_data.append((Path.LINETO, (xdata, ydata)))
+        path_data.append([Path.LINETO, [xdata, ydata]])
         print '  Path.LINETO (%5.2f, %5.2f)'%(xdata, ydata)
-        path_data.append((Path.CLOSEPOLY,  (ptList[0]['xPos'],ptList[0]['yPos'])))
+        path_data.append([Path.CLOSEPOLY,  [ptList[0]['xPos'],ptList[0]['yPos']]])
         print '  Path.CLOSEPOLY (%5.2f %5.2f)'%(ptList[0]['xPos'],ptList[0]['yPos'])
         for pa in patchList:    # Remove previous polygon before adding new path
             pa.remove()
   
     elif nPts >= 4:           # All further points insert LINETO before CLOSEPOLY        
-        path_data[-1] = (Path.LINETO, (xdata, ydata))
+        path_data[-1] = [Path.LINETO, [xdata, ydata]]
         print '  Path.LINETO (%5.2f, %5.2f)'%(xdata, ydata)
-        path_data.append((Path.CLOSEPOLY,  (ptList[0]['xPos'],ptList[0]['yPos'])))
+        path_data.append([Path.CLOSEPOLY,  [ptList[0]['xPos'],ptList[0]['yPos']]])
         print '  Path.CLOSEPOLY (%5.2f %5.2f)'%(ptList[0]['xPos'],ptList[0]['yPos'])
         for pa in patchList:    # Remove previous polygon before adding new path
             pa.remove()
@@ -88,35 +88,43 @@ def extendPolyPath(xdata, ydata):
 
 def editPolyPath(xdata, ydata):
     global ptList, path_data, patchList
-    print '\nin editPolyPath [%5.2f, %5.2f] len=%d'%(xdata,ydata,len(path_data))
+    print '\nin editPolyPath [%5.2f, %5.2f] path_len=%d'%(xdata,ydata,len(path_data))
 
     nPts = len(ptList)
+    print 'nPts = %d' % nPts
     for pt in ptList:
         if pt['selected']:
+            print '  SELECTED'
             if nPts == 0:             # If no points just return
+                print '  nPts = %d'%nPts
                 return
                    
             elif nPts == 1:           # If first point replace with MOVETO
-                path_data[0] = (Path.MOVETO, (xdata, ydata))
+                print '  nPts = %d'%nPts
+                path_data[0] = [Path.MOVETO, [xdata, ydata]]
                 print '  Path.MOVETO (%5.2f, %5.2f)'%(xdata, ydata)
                     
             elif nPts == 2:           # If third point draw LINETO and CLOSEPOLY
+                print '  nPts = %d'%nPts
                 for ix, pt in enumerate(ptList):
+                    print '  ix = %d sel=%r '%(ix, pt['selected'])
                     if pt['selected']:
-                        path_data[ix][1] = (xdata, ydata)
-                        print 'ix %d  Path.MOVETO (%5.2f, %5.2f)'%(ix, xdata, ydata)
+                        print 'XXXXXXXXXXXXXXXXX!!!'
+                        path_data[ix][1] = [xdata, ydata]
+                        print '  ix %d  Path.MOVETO (%5.2f, %5.2f)'%(ix, xdata, ydata)
           
             elif nPts >= 3:           # All further points insert LINETO before CLOSEPOLY        
+                print '  nPts = %d'%nPts
                 for ix, pt in enumerate(ptList):
                     if pt['selected']:
-                        path_data[ix][1] = (xdata, ydata)
+                        path_data[ix][1] = [xdata, ydata]
                         print 'ix %d  Path.MOVETO (%5.2f, %5.2f)'%(ix, xdata, ydata)
                 for pa in patchList:    # Remove previous polygon before adding new path
                     pa.remove()
           
         
-        path_data.append((Path.CLOSEPOLY,  (ptList[0]['xPos'],ptList[0]['yPos'])))
-        print '  Path.CLOSEPOLY (%5.2f %5.2f)'%(ptList[0]['xPos'],ptList[0]['yPos'])
+#        path_data.append((Path.CLOSEPOLY,  (ptList[0]['xPos'],ptList[0]['yPos'])))
+#        print '  Path.CLOSEPOLY (%5.2f %5.2f)'%(ptList[0]['xPos'],ptList[0]['yPos'])
 
     patchList = []
     codes, verts = zip(*path_data)
